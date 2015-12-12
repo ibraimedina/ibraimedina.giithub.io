fe6367e1e4d3388c6adba360d4870a04d256fa31
Ibraim Medina <ibraimedina@gmail.com>
2015-12-12 01:49:03 -0200

    Implementação básica de boas vindas e redes sociais

---
+++ help/config-apache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Apache2 HTTPD default directory
+# /etc/apache2
+
+
+# To confirm cache expirarion, enable module "expires" by "#a2enmod expires"
+# Configure personalized cache to site in sites-available/origami.conf:
+# <Directory /var/www/html/origami>
+#   ExpiresActive on
+#   ExpiresDefault "access plus 1 week"
+# </Directory>
+
+
+
